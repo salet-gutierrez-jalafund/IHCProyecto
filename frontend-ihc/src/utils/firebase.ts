81dc099847d1f2906d135dfa05abc8f7462e17d3
axel-ayala-jalafund <axel.ayala@fundacion-jala.org>
2025-07-12 12:45:35 -0400

    feat: dababase implemented

---
+++ frontend-ihc/src/utils/firebase.ts
@@ -0,0 +1,18 @@
+// src/utils/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBX4ciSN6fgNoAL3ZU8-QzugFqKedqLkDQ",
+  authDomain: "iot-monitoring-a8444.firebaseapp.com",
+  projectId: "iot-monitoring-a8444",
+  storageBucket: "iot-monitoring-a8444.firebasestorage.app",
+  messagingSenderId: "1018108764755",
+  appId: "1:1018108764755:web:29aaab4578855f8b695ca6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
